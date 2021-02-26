821afbff9928f03f0e8e9b5c099f4544f56fbd38
Alan Giacomin <alan.giacomin@gmail.com>
2021-02-26 22:39:14 +0100

    label on left position

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -1,19 +1,37 @@
+// import { classNames } from 'classnames';
 import { ErrorMessage, Field } from 'formik';
 import { PropTypes } from 'prop-types';
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
+const classNames = require('classnames/dedupe');
+
 const Input = (props) => {
-  const { name, placeholder, type, label } = props;
+  const { name, placeholder, type, label, labelLeft, ...others } = props;
+
+  const labelClassName = classNames(
+    {
+      'd-inline': labelLeft,
+      'mr-2': labelLeft,
+    }
+  );
 
   return (
     <>
       <Field name={name}>
         {({ field }) => (
           <Form.Group controlId={`id_${name}`}>
-            {label && <div>{label}</div>}
-            <input {...field} type={type} placeholder={placeholder} />
-            <ErrorMessage name={name} />
+            {label && <div className={labelClassName}>{label}</div>}
+            <Form.Control
+              {...field}
+              {...others}
+              type={type}
+              placeholder={placeholder}
+              className="col-6 d-inline"
+            />
+            <div className="d-inline ml-2">
+              <ErrorMessage name={name} />
+            </div>
           </Form.Group>
         )}
       </Field>
@@ -23,6 +41,7 @@ const Input = (props) => {
 
 Input.propTypes = {
   label: PropTypes.string,
+  labelLeft: PropTypes.bool,
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   type: PropTypes.string,
@@ -30,6 +49,7 @@ Input.propTypes = {
 
 Input.defaultProps = {
   label: null,
+  labelLeft: false,
   placeholder: '',
   type: null,
 };
