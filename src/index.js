8545bafc999b75bea1f363f12a73e013adf02cfd
Alan Giacomin <alan.giacomin@gmail.com>
2021-02-27 23:56:25 +0100

    HttpRequest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Form } from './Form/Form';
+export { default as HttpRequest } from './HttpRequest/HttpRequest';
 export { default as Input } from './Input/Input';
 export { default as Submit } from './Submit/Submit';
