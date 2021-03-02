83f7f91992325165646c52d536f5b829d475a08b
Alan Giacomin <alan.giacomin@gmail.com>
2021-03-02 23:46:36 +0100

    typings

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export { default as Form } from './components/Form/Form';
-export { default as Input } from './components/Input/Input';
-export { default as Submit } from './components/Submit/Submit';
-export { default as httpRequest } from './utils/httpRequest/httpRequest';
-export { default as lazyImport } from './utils/lazyImport/lazyImport';
+export { default as Form } from "./src/components/Form/Form";
+export { default as Input } from "./src/components/Input/Input";
+export { default as Submit } from "./src/components/Submit/Submit";
+export { default as httpRequest } from "./src/utils/httpRequest/httpRequest";
+export { default as lazyImport } from "./src/utils/lazyImport/lazyImport";
