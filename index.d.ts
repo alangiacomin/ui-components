b780076a212327167f42f726eb818cad91f479a8
Alan Giacomin <alan.giacomin@gmail.com>
2021-03-03 22:56:45 +0100

    script createIndex

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as Form } from "./src/components/Form/Form";
-export { default as Input } from "./src/components/Input/Input";
-export { default as Submit } from "./src/components/Submit/Submit";
-export { default as httpRequest } from "./src/utils/httpRequest/httpRequest";
-export { default as lazyImport } from "./src/utils/lazyImport/lazyImport";
